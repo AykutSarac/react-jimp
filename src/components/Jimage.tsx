12b5816d00b2804069b81741c171aa838779fe9a
AykutSarac <aykutsarac0@gmail.com>
2021-11-10 22:49:19 +0300

    update packages

--- src/components/Jimage.tsx
+++ src/components/Jimage.tsx
@@ -29,31 +29,6 @@ const Jimage: React.FC<Options> = (props) => {
         jimpImage[Effect]();
       }
 
-      /* for (const option in jimp_actions) {
-        if (typeof loadImage[option] !== 'function') continue;
-        const IMG_PARAMS = options[option];
-
-        if (typeof IMG_PARAMS === 'boolean') {
-          // Perform if boolean true
-          if (IMG_PARAMS === true) loadImage[option]();
-        } else if (IMG_PARAMS.includes('true')) {
-          // Get parameters as boolean
-          const GET_PARAMS = IMG_PARAMS.split(',');
-          const BOOL_PARAMS = GET_PARAMS.map((bool: string) => bool.includes('true'));
-          loadImage[option](...BOOL_PARAMS);
-        } else if (option === ) {
-          // Color manipulation
-          loadImage.color(options[option]);
-        } else {
-          // Take parameters and convert to int
-          const PARAMS_ARR = IMG_PARAMS.split(',');
-          const FLOAT_PARAMS = PARAMS_ARR.map((opt: string) => parseFloat(opt));
-
-          // Perform method
-          loadImage[option](...FLOAT_PARAMS);
-        }
-      } */
-
       const mime = await jimpImage.getBase64Async(Jimp.MIME_JPEG);
       setLoading(false);
       setImage(mime);
